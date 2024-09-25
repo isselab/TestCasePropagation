508e12582999ec4ea124f94dcfbe5def135e96fb
Luca Kramer <Luca.Kramer@rub.de>
2024-09-25 22:09:31 +0200

    Added Copyright to files

--- src/main/java/se/isselab/testcasepropagation/intelliJ/visualize/CodeDifferenceViewer.java
+++ src/main/java/se/isselab/testcasepropagation/intelliJ/visualize/CodeDifferenceViewer.java
@@ -1,3 +1,18 @@
+/*
+Copyright [2024] [Luca Kramer]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package se.isselab.testcasepropagation.intelliJ.visualize;
 
 import com.intellij.diff.DiffManager;
@@ -40,7 +55,6 @@ public class CodeDifferenceViewer {
         // Get the virtual file from the temporary file
         tempVirtualFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(tempFile);
         file1 = virtualFile;
-        System.out.println(file1);
 
         if(file1 == null) {
             FileCreator fileCreator = new FileCreator(project);
@@ -48,7 +62,6 @@ public class CodeDifferenceViewer {
             FileFinder fileFinder = new FileFinder();
             file1 = fileFinder.findTestFileRecursively(testedClass);
         }
-        System.out.println(file1);
 
         if (tempVirtualFile != null) {
             // Create and show the diff request
@@ -122,7 +135,6 @@ public class CodeDifferenceViewer {
                     String newContent = new StringBuilder(originalContent)
                             .insert(lastBraceIndex, contentToInsert)
                             .toString();
-
                     // Update the document with the new content
                     document1.setText(newContent);
                     FileDocumentManager.getInstance().saveDocument(document1);
